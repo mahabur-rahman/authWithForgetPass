e91eb85dd215fbe1437fdaab144a73d86051e5d0
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-05-08 19:44:01 +0600

    valid user api call with frontend and check token

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const history = useNavigate();
+
+  const DashboardValidUser = async () => {
+    let token = localStorage.getItem("usersdatatoken");
+    // console.log(token);
+
+    const res = await fetch(`/validuser`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+    });
+
+    const data = await res.json();
+    // console.log(data);
+    if (data.status === 401 || !data) {
+      history("*");
+    } else {
+      console.log(`User verify`);
+      history("/dashboard");
+    }
+  };
+
+  useEffect(() => {
+    DashboardValidUser();
+  }, []);
+
   return (
     <>
       <img src="./man.jpg" style={{ width: "200px", marginTop: 20 }} alt="" />
